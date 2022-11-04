9a5a95cc863f33453b345889ffe823424e824762
Reynan Decena <reynandecena1984@gmail.com>
2022-11-04 20:21:00 +0800

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
+const Task = require("./models/taskModel");
+const taskRoutes = require("./routes/taskRoute");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"],
+  })
+);
+
+app.use("/api/tasks", taskRoutes);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+const PORT = process.env.PORT || 4000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
