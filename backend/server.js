5422051467037b2920f32643cedcc640b47d4b00
Reynan Decena <reynandecena1984@gmail.com>
2022-11-04 22:04:08 +0800

    New Commit

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://mern-task-app-frm3.onrender.com",
-    ],
+    origin: ["http://localhost:3000", "https://rcreate-task-app.onrender.com"],
   })
 );
 
